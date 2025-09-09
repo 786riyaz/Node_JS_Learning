f179903e71b13f8ba2c4a1422f91c48f529791c0
Riyaz <moh.riyazpathan@gmail.com>
2025-09-09 13:49:23 +0530

    Added Global Modules

---
+++ 1 NodeJS Basics/3 Global Modules/index.js
@@ -0,0 +1,6 @@
+/*
+  __dirname is a global module which provides Directory Name
+  __filename is a global mosule which provides File Name
+*/
+console.log("Directory Name ::",__dirname);
+console.log("File Name ::",__filename);
