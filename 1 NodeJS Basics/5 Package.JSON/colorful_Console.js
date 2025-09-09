864bff8005e2378034b449c5e368b440fcdd46b4
Riyaz <moh.riyazpathan@gmail.com>
2025-09-09 13:50:28 +0530

    Added Notes and example for package.json

---
+++ 1 NodeJS Basics/5 Package.JSON/colorful_Console.js
@@ -0,0 +1,33 @@
+var colors = require('colors');
+// colors.enable();     // -- To enable the Color Module Functionality
+// colors.disable();    // -- To disable the color module functinality
+
+console.log("Hello World in Red Font".red);
+console.log("Hello World in Bright Green".brightGreen);
+console.log("Hello World in Bright Blue".brightBlue);
+console.log("Hello World in Bright White".brightWhite);
+console.log();
+
+console.log("Hello World in Green BackGround".bgGreen);
+console.log("Hello World in Gray BackGround".bgGray);
+console.log("Hello World in Magenta BackGround".bgMagenta);
+console.log("Hello World in White BackGround".bgWhite);
+console.log();
+
+console.log("Hello World in Bright Red BackGround".bgBrightRed);
+console.log("Hello World in Bright Blue BackGround".bgBrightBlue);
+console.log();
+
+console.log("Hello World in Bold Style".bold);
+console.log("Hello World in Dim Style".dim);
+console.log("Hello World in Stricke Though Style".strikethrough);
+console.log("Hello World in Underline Style".underline);
+console.log();
+
+console.log("Hello World in Rainbow Style".rainbow);
+console.log("Hello World in Zebra Style".zebra);
+console.log("Hello World in America Style".america);
+console.log("Hello World in Trap Style".trap);
+console.log();
+
+console.log("Hello World in Random Style".random);
