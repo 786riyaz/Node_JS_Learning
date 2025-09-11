edd200a0bec9cce90bbe04f8d18588aa5d39b79a
Riyaz <moh.riyazpathan@gmail.com>
2025-09-11 12:06:05 +0530

    Added Code of Generating Files Name List

---
+++ 1 NodeJS Basics/8 File System/3 Generating Files List/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = path.join(__dirname,"Text_Files");
+console.log("Directory Path ::", dirPath);
+
+fs.readdir(dirPath,(err,files)=>{
+    console.log("List of Files ::: ");
+    console.log(JSON.stringify(files));
+})
