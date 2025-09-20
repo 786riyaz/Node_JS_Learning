b441f27a5dd2ccee0856f3ea73326e298069c882
Riyaz <moh.riyazpathan@gmail.com>
2025-09-20 12:35:16 +0530

    Added Code for Updating a record with condition in mongo db using NodeJS

---
+++ 11 MondoDB CRUD/7 Update One With Condition/3_UpdateOne_With_Condition.js
@@ -0,0 +1,22 @@
+const connectToDatabase = require('./mongodb');         // here we have to use ./ to point to the javascript file instead of mongodb package
+
+const updateOne = async () => {
+    // Get the collection object
+    let collection = await connectToDatabase();
+
+    // Update one document of the 'Products' collection
+    let result = await collection.updateOne(
+        { "ProductName": "Acer Predator" },
+        { $set: { "Price": 400, "Category":"Laptop" } }
+    );
+    console.log("Result ::", result);
+
+    if (result.acknowledged) {
+        console.log("Single Record Update Successfully");
+    } else {
+        console.error("Error while updating the record");
+    }
+}
+
+// Execute the main function
+updateOne();
