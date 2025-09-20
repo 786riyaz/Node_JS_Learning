12d9cacdf579bc094c4be95455b5a2686f9d1ad0
Riyaz <moh.riyazpathan@gmail.com>
2025-09-20 12:34:10 +0530

    Added Code for Read One Record from Mongo DB using Node JS

---
+++ 11 MondoDB CRUD/3 Read One Record Without Condition/2_Read_One_Data_From_Collection.js
@@ -0,0 +1,15 @@
+const connectToDatabase = require('./mongodb');         // here we have to use ./ to point to the javascript file instead of mongod b package
+
+const main = async () => {
+    // Get the collection object
+    let collection = await connectToDatabase();
+
+    // Fetch one document from the 'Products' collection
+    let data = await collection.findOne();
+
+    // Log the fetched data
+    console.log('Products Data:', data);
+}
+
+// Execute the main function
+main();
