34cf0b13b2fdfcb78a196145e7307e25fb7682d7
Riyaz <moh.riyazpathan@gmail.com>
2025-09-21 14:16:45 +0530

    Added Code for Update One and Many Records in Mongo DB

---
+++ 11 MondoDB CRUD/8 Update One Without Condition/3_UpdateOne_Without_Condition.js
@@ -0,0 +1,22 @@
+const connectToDatabase = require('./mongodb');         // here we have to use ./ to point to the javascript file instead of mongodb package
+
+const updateOne = async () => {
+    // Get the collection object
+    let collection = await connectToDatabase();
+
+    // Update one document of the 'Products' collection
+    let result = await collection.updateOne(
+        {  },
+        { $set: { "Test": 400, } }
+    );
+    console.log("Result ::", result);
+
+    if (result.acknowledged) {
+        console.log("Single Record Update Successfully");
+    } else {
+        console.error("Error while updating the record");
+    }
+}
+
+// Execute the main function
+updateOne();
---
+++ 11 MondoDB CRUD/9 Update Many Without Condition/3_UpdateMany_Without_Condition.js
@@ -0,0 +1,22 @@
+const connectToDatabase = require('./mongodb');         // here we have to use ./ to point to the javascript file instead of mongodb package
+
+const updateOne = async () => {
+    // Get the collection object
+    let collection = await connectToDatabase();
+
+    // Update many documents of the 'Products' collection
+    let result = await collection.updateMany(
+        {  },
+        { $set: { "Test": 786 } }
+    );
+    console.log("Result ::", result);
+
+    if (result.acknowledged) {
+        console.log("Single Record Update Successfully");
+    } else {
+        console.error("Error while updating the record");
+    }
+}
+
+// Execute the main function
+updateOne();
