17eded2079a8ba5b5db812b3bea645071e1aec87
Riyaz <moh.riyazpathan@gmail.com>
2025-09-24 13:47:28 +0530

    Added Code for Event and Event Emitter in Node JS

---
+++ 17 Event & EventEmitter/index.js
@@ -0,0 +1,32 @@
+const express = require ("express");
+const app = express ();
+
+const EventEmitter = require ("events");
+const event = new EventEmitter();
+
+let counter = 0;
+event.on("countAPI", ()=>{
+    counter++;
+    console.log("Inside the count API Event ==> Counter :", counter);
+
+})
+
+app.get('/',(req,res)=>{
+    console.log("Inside the Get Fuction");
+    res.send("API Called");
+    event.emit("countAPI");
+});
+
+app.post('/create',(req,res)=>{
+    console.log("Inside the Post Fuction");
+    res.send("API Called");
+    event.emit("countAPI");
+});
+
+app.put('/update',(req,res)=>{
+    console.log("Inside the Put Fuction");
+    res.send("API Called");
+    event.emit("countAPI");
+});
+
+app.listen(786);
