aece78500c4921adfbf9a4121e97e2709506d698
Riyaz <moh.riyazpathan@gmail.com>
2025-10-25 11:33:21 +0530

    Added Notes and codes for Asynchronous Basics and renamed the folder

--- 13 Mongoose Basics/Mongoose_Example-Tasin-Laptop.js
+++ 13 Mongoose Basics/Mongoose_Example-Tasin-Laptop.js
@@ -16,8 +16,8 @@ const Products = mongoose.model("mongoose_test", productsSchema);
 
 // Create a new document
 const newProduct = new Products({ ProductName: "Test Item 10", Category: "Test", "Brand" : "Test", Price : 1234, "Test":"Test" });    // Here the "Test":"Test" will be ignore as per the strict mode
-
 // newProduct.save().then(() => console.log("Product saved"));
+
 
 newProduct.save()
   .then((savedDoc) => console.log("Product saved:", savedDoc))
